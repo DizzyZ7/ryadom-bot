3f05a65faf6f982157203fbec502718ce96ad8d9
Dimash Janibekov <99655528+DizzyZ7@users.noreply.github.com>
2026-06-07 15:16:59 +0300

    Connect admin location handlers

--- app/all_handlers.py
+++ app/all_handlers.py
@@ -5,6 +5,7 @@ from app.admin_user_handlers import admin_user_router
 from app.admin_complaint_handlers import admin_complaint_router
 from app.admin_stats_handlers import admin_stats_router
 from app.admin_audit_handlers import admin_audit_router
+from app.admin_location_handlers import admin_location_router
 from app.complaint_handlers import complaint_router
 from app.location_handlers import location_router
 from app.me_handlers import me_router
@@ -18,6 +19,7 @@ router.include_router(admin_user_router)
 router.include_router(admin_complaint_router)
 router.include_router(admin_stats_router)
 router.include_router(admin_audit_router)
+router.include_router(admin_location_router)
 router.include_router(complaint_router)
 router.include_router(location_router)
 router.include_router(me_router)
