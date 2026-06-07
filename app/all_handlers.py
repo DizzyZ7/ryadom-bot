6a7bd0d5e5a9e2ba20d168a2b1da1683aa79d7ff
Dimash Janibekov <99655528+DizzyZ7@users.noreply.github.com>
2026-06-07 13:09:09 +0300

    Connect admin user handlers

--- app/all_handlers.py
+++ app/all_handlers.py
@@ -1,9 +1,11 @@
 from aiogram import Router
 from app.handlers_fixed import router as user_router
 from app.admin_handlers import admin_router
+from app.admin_user_handlers import admin_user_router
 from app.complaint_handlers import complaint_router
 
 router = Router()
 router.include_router(admin_router)
+router.include_router(admin_user_router)
 router.include_router(complaint_router)
 router.include_router(user_router)
